5d677baa2cf3e091113cdad088ab90994e9a714b
pnadolny13 <pnadolny@deloitte.com>
2018-02-12 22:49:00 -0500

    Start Over From Scratch

--- firstsite/Budget/urls.py
+++ firstsite/Budget/urls.py
@@ -4,19 +4,35 @@ from . import views
 app_name = 'Budget'
 
 urlpatterns = [
+        
+    ################   LEGACY    #################    
+    #/budget/
+#    url(r'^$', views.index, name='index'),
+#    # /budget/<budget.id>/
+#    url(r'^(?P<budget_id>[0-9]+)/$', views.detail, name='detail'),
+#    # /budget/June/
+#    url(r'^(?P<month_id>[\w\-]+)/$', views.months, name='months'),
+#    # /budget/<budget.id>/favorite, does some logic and sends back
+#    url(r'^(?P<month_id>[\w\-]+)/$', views.months, name='months'),
+#    # /budget/generic.html
+#    url(r'^generic.html$', views.generic, name='generic'),
+#    # /budget/elements.html
+#    url(r'^elements.html$', views.elements, name='elements'),
+#    # /budget/all_months.html
+#    url(r'^all_months.html$', views.all_months, name='all_months'),
+
+
+
+    ##################   NEW     ####################
     #/budget/
     url(r'^$', views.index, name='index'),
-    # /budget/<budget.id>/
-    url(r'^(?P<budget_id>[0-9]+)/$', views.detail, name='detail'),
-    # /budget/June/
-    url(r'^(?P<month_id>[\w\-]+)/$', views.months, name='months'),
-    # /budget/<budget.id>/favorite, does some logic and sends back
-    url(r'^(?P<month_id>[\w\-]+)/$', views.months, name='months'),
-    # /budget/generic.html
-    url(r'^generic.html$', views.generic, name='generic'),
-    # /budget/elements.html
-    url(r'^elements.html$', views.elements, name='elements'),
-    # /budget/all_months.html
-    url(r'^all_months.html$', views.all_months, name='all_months'),
-
+    
+    # /budget/track
+    url(r'^track$', views.track, name='track'),
+    
+    # /budget/analyze
+    url(r'^analyze$', views.analyze, name='analyze'),
+    
+    # /budget/take-action
+    url(r'^action$', views.action, name='action'),    
 ]
--- firstsite/Budget/views.py
+++ firstsite/Budget/views.py
@@ -6,34 +6,52 @@ from django.shortcuts import render, get_object_or_404
 from .models import Expense
 
 
+    ################   LEGACY    #################    
+
+#def index(request):
+#   all_budget = Expense.objects.all()
+#   return render(request, 'Budget/index.html', {'all_budget': all_budget})
+#
+#
+#def detail(request, budget_id):
+#    expense = get_object_or_404(Expense, pk=budget_id)
+#    return render(request, 'Budget/detail.html', {'expense': expense})
+#
+#def months(request, month_id):
+#    month_id = month_id.lower()
+#    diction = {'january':'01','february':'02','march':'03','april':'04','may':'05','june':'06','july':'07','august':'08','september':'09','october':'10','november':'11','december':'12'}
+#    try:
+#        diction[month_id]
+#        convert = diction[month_id]
+#        inputs_month = {"category": "testCat"}
+#        #inputs_month = Expense.objects.filter(date__month=convert)
+#        return render(request, 'Budget/months.html', {'inputs_month': inputs_month})
+#    except KeyError:
+#        raise Http404(month_id + ' is not a month!')
+#
+#def generic(request):
+#    return render(request, 'Budget/generic.html')
+#
+#def elements(request):
+#    return render(request, 'Budget/elements.html')
+#
+#def all_months(request):
+#    months = ['January','February','March', 'April', 'May','June','July','August','September','October','November','December'];
+#    return render(request, 'Budget/all_months.html', {'months' : months})
+
+
+
+    ##################   NEW     ####################
 
 def index(request):
-   all_budget = Expense.objects.all()
-   return render(request, 'Budget/index.html', {'all_budget': all_budget})
+   return render(request, 'Budget/index.html')
 
+def track(request):
+   return render(request, 'Budget/track.html')
 
-def detail(request, budget_id):
-    expense = get_object_or_404(Expense, pk=budget_id)
-    return render(request, 'Budget/detail.html', {'expense': expense})
+def analyze(request):
+   return render(request, 'Budget/analyze.html')
 
-def months(request, month_id):
-    month_id = month_id.lower()
-    diction = {'january':'01','february':'02','march':'03','april':'04','may':'05','june':'06','july':'07','august':'08','september':'09','october':'10','november':'11','december':'12'}
-    try:
-        diction[month_id]
-        convert = diction[month_id]
-        inputs_month = {"category": "testCat"}
-        #inputs_month = Expense.objects.filter(date__month=convert)
-        return render(request, 'Budget/months.html', {'inputs_month': inputs_month})
-    except KeyError:
-        raise Http404(month_id + ' is not a month!')
+def action(request):
+   return render(request, 'Budget/action.html')
 
-def generic(request):
-    return render(request, 'Budget/generic.html')
-
-def elements(request):
-    return render(request, 'Budget/elements.html')
-
-def all_months(request):
-    months = ['January','February','March', 'April', 'May','June','July','August','September','October','November','December'];
-    return render(request, 'Budget/all_months.html', {'months' : months})
