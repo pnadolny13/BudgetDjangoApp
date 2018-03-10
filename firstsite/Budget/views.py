c7b53fdda92ea15e36c686038ce7276fe2e84df9
pnadolny13 <pnadolny@deloitte.com>
2018-03-10 15:14:24 -0500

    Analyze Page - Percentages and Form for Selecting Month

--- firstsite/Budget/views.py
+++ firstsite/Budget/views.py
@@ -10,8 +10,9 @@ import pandas
 import datetime
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
-from .forms import ExpenseForm
-from .models import UserInput
+from django.db.models import Sum
+from .forms import ExpenseForm, MonthListForm
+from .models import UserInput, UserCategories
 from django_tables2 import RequestConfig
 from .tables import UserInputTable
 
@@ -25,6 +26,7 @@ def track(request):
     if request.method == 'POST':
         # validate form first
         form = ExpenseForm(request.POST, user = request.user)
+        today = datetime.date.today()
         if form.is_valid():
            label = request.POST.get('label')
            inputDate = request.POST.get('date')
@@ -47,7 +49,7 @@ def track(request):
 
         # get 10 entries
         qs = UserInputTable(UserInput.objects.order_by("-date").filter(user=currentUser).filter(date__gte=month))
-
+     
         # to allow for sorting 
         # RequestConfig(request).configure(qs)
 
@@ -59,7 +61,48 @@ def track(request):
         return redirect('accounts/login')
 
 def analyze(request):
-   return render(request, 'Budget/analyze.html')
+    # I will show all categories by user plus tax and pay
+    # with monthly saved budget settings
+    today = datetime.date.today()
+    month= today.replace(day=1)
+    if request.method == 'POST':
+        # validate form first
+        form = MonthListForm(request.POST)
+        today = datetime.date.today()
+        if form.is_valid():
+            selected_month = request.POST.get('month')
+            month = month.replace(month=int(selected_month))
+    
+    currentUser = request.user.username
+    cats = UserCategories.objects.filter(user=currentUser)
+    if currentUser != "admin":
+        cats += UserCategories.objects.filter(user="admin")
+    
+    # get sum of amounts this month, in each category, then add to list
+
+    totals_dict = {}
+    total = UserInput.objects.filter(user=currentUser).filter(date__gte=month).aggregate(Sum('amount'))
+    total_val = str(total['amount__sum'])
+    for category in cats:
+        cat_sum = UserInput.objects.filter(user=currentUser).filter(date__gte=month).filter(category=category).aggregate(Sum('amount'))
+
+        value = cat_sum['amount__sum']
+        if value is None:
+            value = 0
+            percent = 0
+        else:
+            percent = str(round((100 * float(value)/float(total_val)),0))
+        totals_dict[str(category)] = (str(value) + " - " + str(percent) + "%")
+    # then they can play with those numbers percent or dollar amount
+    # see monthly flow and save budget settings
+
+    # then we will have same setup with monthly total percentages so far and remaining balance
+    # allow them to put in expected additional amounts
+
+    # view by category like my excel sheet monthly - adjust date range
+
+    # weekly budgets, color by how well you did, then click in to see all details
+    return render(request, 'Budget/analyze.html', {'totals_dict': totals_dict, 'form' : MonthListForm(initial={ 'month': today.month})})
 
 def action(request):
    return render(request, 'Budget/action.html')
