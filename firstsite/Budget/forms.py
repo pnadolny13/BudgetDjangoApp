7086473d761e92885f47f9c4b95d89456d2044a1
pnadolny13 <pnadolny@deloitte.com>
2018-03-11 19:52:11 -0400

    Forms

--- firstsite/Budget/forms.py
+++ firstsite/Budget/forms.py
@@ -14,3 +14,6 @@ class ExpenseForm(forms.Form):
             user = kwargs.pop('user')
             super(ExpenseForm, self).__init__(*args, **kwargs)
             self.fields["category"].queryset = UserCategories.objects.filter(user__in=[user, "admin"])
+
+class MonthListForm(forms.Form):
+        month= forms.CharField(label='Analyze other month?', widget=forms.Select(choices=[ ('1', 'January'), ('2', 'February'), ('3', 'March'), ('4', 'April'), ('5', 'May'), ('6', 'June'), ('7', 'July'), ('8', 'August'), ('9', 'September'), ('10', 'October'),('11', 'November'), ('12', 'December')]))
