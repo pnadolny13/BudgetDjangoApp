65bf81a5bb766e0a5d4024fbbb993efb0adf705b
pnadolny13 <pnadolny@deloitte.com>
2018-03-14 22:02:24 -0400

    Analyze Table - v1

--- firstsite/Budget/views.py
+++ firstsite/Budget/views.py
@@ -15,56 +15,63 @@ from .forms import ExpenseForm, MonthListForm
 from .models import UserInput, UserCategories
 from django_tables2 import RequestConfig
 from .tables import UserInputTable
+import itertools
+from calendar import monthrange
 
+##################   NEW     ####################
 
-    ##################   NEW     ####################
 
 def index(request):
-   return render(request, 'Budget/index.html')
+    return render(request, 'Budget/index.html')
+
 
 def track(request):
     if request.method == 'POST':
         # validate form first
-        form = ExpenseForm(request.POST, user = request.user)
+        form = ExpenseForm(request.POST, user=request.user)
         today = datetime.date.today()
         if form.is_valid():
-           label = request.POST.get('label')
-           inputDate = request.POST.get('date')
-           amount = request.POST.get('amount')
-           category = request.POST.get('category')
-           comment = request.POST.get('comment')
-           entry = UserInput(user= request.user.username, date=inputDate, amount=amount, category=category, expenseLabel=label, comment=comment)
-           entry.save()
-           return redirect('/budget/track')
+            label = request.POST.get('label')
+            inputDate = request.POST.get('date')
+            amount = request.POST.get('amount')
+            category = request.POST.get('category')
+            comment = request.POST.get('comment')
+            entry = UserInput(user=request.user.username, date=inputDate,
+                              amount=amount, category=category, expenseLabel=label, comment=comment)
+            entry.save()
+            return redirect('/budget/track')
         else:
             # this doesnt load a form it just says failed
-            return render(request, 'Budget/track.html', {'notification': "Failed to Track"}, {'form': ExpenseForm(user = request.user, initial={ 'date': today, 'comment': 'optional'})})
+            return render(request, 'Budget/track.html', {'notification': "Failed to Track"}, {'form': ExpenseForm(user=request.user, initial={'date': today, 'comment': 'optional'})})
     elif request.user.is_authenticated:
         # get user name
         currentUser = request.user.username
-        
+
         # get first day of current month
         today = datetime.date.today()
-        month= today.replace(day=1)
+        month = today.replace(day=1)
 
         # get 10 entries
-        qs = UserInputTable(UserInput.objects.order_by("-date").filter(user=currentUser).filter(date__gte=month))
-     
-        # to allow for sorting 
+        qs = UserInputTable(UserInput.objects.order_by(
+            "-date").filter(user=currentUser).filter(date__gte=month))
+
+        # to allow for sorting
         # RequestConfig(request).configure(qs)
 
-        form = ExpenseForm(user = request.user, initial={ 'date': today, 'comment': 'optional'})
+        form = ExpenseForm(user=request.user, initial={
+                           'date': today, 'comment': 'optional'})
         return render(request, 'Budget/track.html', {'qs': qs, 'form': form})
 
     else:
         # you shouldnt have gotten this far without login but check anyways
         return redirect('accounts/login')
 
+
 def analyze(request):
     # I will show all categories by user plus tax and pay
     # with monthly saved budget settings
     today = datetime.date.today()
-    month= today.replace(day=1)
+    month = today.replace(day=1)
     if request.method == 'POST':
         # validate form first
         form = MonthListForm(request.POST)
@@ -72,20 +79,22 @@ def analyze(request):
         if form.is_valid():
             selected_month = request.POST.get('month')
             month = month.replace(month=int(selected_month))
-    
+
     currentUser = request.user.username
     cats = UserCategories.objects.filter(user=currentUser)
     if currentUser != "admin":
         cats += UserCategories.objects.filter(user="admin")
-    
+
     # get sum of amounts this month, in each category, then add to list
 
     totals_dict = {}
-    total = UserInput.objects.filter(user=currentUser).filter(date__gte=month).filter(category='Income').aggregate(Sum('amount'))
+    total = UserInput.objects.filter(user=currentUser).filter(
+        date__gte=month).filter(category='Income').aggregate(Sum('amount'))
     total_income = str(total['amount__sum'])
     total_spent = 0
     for category in cats:
-        cat_sum = UserInput.objects.filter(user=currentUser).filter(date__gte=month).filter(category=category).aggregate(Sum('amount'))
+        cat_sum = UserInput.objects.filter(user=currentUser).filter(
+            date__gte=month).filter(category=category).aggregate(Sum('amount'))
 
         value = cat_sum['amount__sum']
         # if no data for that category then we cant give data
@@ -97,12 +106,66 @@ def analyze(request):
             value = 0
             percent = 0
         else:
-            percent = str(round((100 * float(value)/float(total_income)),0))
+            percent = str(round((100 * float(value)/float(total_income)), 0))
         # dont include income
         if str(category) == 'Income':
             continue
-        totals_dict[str(category)] = [("$ " + str(value)), (str(percent) + "%")]
+        totals_dict[str(category)] = [
+            ("$ " + str(value)), (str(percent) + "%")]
         total_spent += value
+
+    # get all month data for each category (amount, date, category, and description), use category to organize
+    # them in columns and date to organize by week for left set
+
+    count = 1
+    masterWeekList = []
+    totalDays = monthrange(today.year, today.month)[1]
+    print("TOTALDAYS " + str(totalDays))
+    while count < 5:
+        endDay = int(round((totalDays/4)*count))
+        startDay = int(round((totalDays/4)*count-(totalDays/4))+1)
+        if count == 4:
+            endDay = totalDays
+        if startDay > totalDays:
+            startDay = 1
+        # elif startDay == 0:
+        #     startDay = 1
+        endDate = today.replace(day=endDay)
+        startDate = today.replace(day=startDay)
+        print("START " + str(startDate) + " END : " +
+              str(endDate) + "COUNT " + str(count))
+        # master list of all lists of category specific lists by week
+        weekList = []
+        for category in cats:
+            print("category: " + str(category))
+            weekCatList = []
+            print("before week cat list: " + str(weekCatList))
+            weekData = UserInput.objects.filter(user=currentUser).filter(
+                date__range=[startDate, endDate]).filter(category=category)
+            for element in weekData:
+                print("weekData " + str(weekData))
+                # this looks like -- weekData<QuerySet [<UserInput: UserInput object (13)>]>
+                # print("weekData" + str(list(weekData)))
+                # this looks like vvv elementUserInput object (13)
+                # print("element" + str(element.amount))
+                # el = weekData[element]
+                # print("EL" + str(el))
+                amount = element.amount
+                expenseLabel = element.expenseLabel
+                combo = str(amount) + "-" + str(expenseLabel) + "-" + str(element.category)
+                print("adding " + str(combo))
+                weekCatList.append(str(combo))
+                print("weekCatList: " + str(weekCatList))
+            # weekList[count]
+            # weekList.insert(count, weekCatList)
+            weekList.append(weekCatList)
+        print("WEEKLIST PRIOR TO PIVOT: " + str(weekList))
+        weekList = list(itertools.zip_longest(*weekList))
+        masterWeekList.append(weekList)
+        # del weekList[:]
+
+        count += 1
+    print("THIS IS THE MASTER WEEK LIST" + str(masterWeekList))
 
     # then they can play with those numbers percent or dollar amount
     # see monthly flow and save budget settings
@@ -114,11 +177,13 @@ def analyze(request):
 
     # weekly budgets, color by how well you did, then click in to see all details
     savings = float(total_income) - float(total_spent)
-    save_percent = str(round((100 * float(savings)/float(total_income)),0))
-    return render(request, 'Budget/analyze.html', {'totals_dict': totals_dict, 'form' : MonthListForm(initial={ 'month': today.month}), 'income': ('$ ' + total_income), 'savings': ('$ ' + str(savings)), 'save_percent': (str(save_percent) + '%')})
+    save_percent = str(round((100 * float(savings)/float(total_income)), 0))
+    return render(request, 'Budget/analyze.html', {'totals_dict': totals_dict, 'form': MonthListForm(initial={'month': today.month}), 'income': ('$ ' + total_income), 'savings': ('$ ' + str(savings)), 'save_percent': (str(save_percent) + '%'), 'monthTable': masterWeekList})
+
 
 def action(request):
-   return render(request, 'Budget/action.html')
+    return render(request, 'Budget/action.html')
+
 
 def signup(request):
     if request.method == 'POST':
